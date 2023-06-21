4c9c749d9f5ac4b5f0d7d0fa3a512f30015400ea
ruMode <116144610+ruMode@users.noreply.github.com>
2023-06-21 10:34:47 +0300

    Update MainActivity.cs

--- DigitalStudentTicket.Android/MainActivity.cs
+++ DigitalStudentTicket.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace DigitalStudentTicket.Droid
 {
-    [Activity(Label = "KPK mobile", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme.Splash", MainLauncher = true, ScreenOrientation =ScreenOrientation.Portrait, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Эл. студенческий", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ScreenOrientation =ScreenOrientation.Portrait, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
